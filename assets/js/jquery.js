212dfe27bebfdc4ff944f0d41bb7ae6dc3634859
seth rojas <p>
2023-07-17 15:46:55 -0400

    last changes

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     $("#enviar").click(function () {
         alert("El correo fue enviado exitosamente, nos pondremos en contacto pronto...");
     });
-    $('#plane').popover({ content: "Somos viajeros a destinos increibles y que nos motiva a compartir lo glorioso de nuestro país", placement: "top", trigger: "hover" });
-    $('#montaña').popover({ content: "Somos amantes de lo natural y la belleza de nuestro territorio", placement: "bottom", trigger: "hover" });
-    $('#ruta').popover({ content: "Somos aventureros de las rutas más hermosas de nuestros parques nacionales", placement: "top", trigger: "hover" });
+    $('#plane').popover({ content: "Blog de viajes por Chile: una recopilación de destinos y lugares que ver en Chile a partir de los meses que pasé viajando por el país y el tiempo.", placement: "top", trigger: "hover" });
+    $('#montaña').popover({ content: "Chile es mi debilidad y uno de los países más fascinantes de norte a sur, del desierto a los hielos del sur.", placement: "bottom", trigger: "hover" });
+    $('#ruta').popover({ content: "Hablar de si es o no seguro viajar a Sudamérica es, literalmente, meter el pie en el barro. ¿Por qué? Porque decir Sudamérica.", placement: "top", trigger: "hover" });
 });
